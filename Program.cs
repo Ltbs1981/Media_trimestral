e69a1a295c58583b06ff80fd9e0ec989cead14ff
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-10 19:36:46 -0300

    Fase 1, Início projeto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mediaTrimestral
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
