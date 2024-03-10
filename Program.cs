f5f79e05b5d37414ab942e0f2e668c8471a3b4b8
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-10 20:46:57 -0300

    Programa estruturado

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace mediaTrimestral
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Programa cálculo de médias trimestrais.");
+            Console.WriteLine("Digite a nota entre 0 e 10 do 1º trimestre");
+            double nt1 = double.Parse(Console.ReadLine())*3.0;
+            Console.WriteLine("Digite a nota entre 0 e 10 do 2º trimestre");
+            double nt2 = double.Parse(Console.ReadLine()) * 3.5;
+            Console.WriteLine("Digite a nota entre 0 e 10 do 3º trimestre");
+            double nt3 = double.Parse(Console.ReadLine()) * 3.5;
+
+            double media = nt1 + nt2 + nt3;
+
+            if ( media >= 60)
+            {
+                Console.WriteLine("Aluno aprovado");
+
+            }else
+            {
+                Console.WriteLine("O aluno não está aprovado.");
+                double pontosFaltando = 100 - media;
+
+                Console.WriteLine($"\n Estão faltando: {pontosFaltando} Pontos");
+
+            }
+
+            Console.WriteLine(media);
+
         }
     }
 }
