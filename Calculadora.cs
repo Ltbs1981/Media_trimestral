546b9053460dbecc234792943ffd79d56806d499
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-03-15 00:50:58 -0300

    Corrigindo bugs

--- Calculadora.cs
+++ Calculadora.cs
@@ -4,8 +4,9 @@ namespace mediaTrimestral
 {
     class Calculadora
     {
-        public double nota1, nota2, nota3;
-
+        public double nota1;
+        public  double nota2;
+        public double nota3;
         public double CalcularMedia()
         {
             return nota1 + nota2 + nota3; 
